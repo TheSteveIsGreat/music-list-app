48434d25be56f4766cc2a6ee7f5acaff9779e536
Steve Crass <stevecrass@Saras-MBP.hsd1.ut.comcast.net>
2022-03-16 15:43:31 -0600

    SongList done

---
+++ src/class/ClassSongList.js
@@ -0,0 +1,79 @@
+import React from "react";
+
+// Convention for class component
+class ClassSongList extends React.Component {
+  constructor(props) {
+    super(props)
+    // Since React only, setting state w/ song data 
+    // to populate form
+    this.state = {
+      songs: [
+        {title: 'Get Back', artist: 'The Beatles'},
+        {title: 'Hey Jude', artist: 'The Beatles'},
+        {title: 'Paperback Writer', artist: 'The Beatles'},
+        {title: 'Thunderstruck', artist: 'AC/DC'},
+        {title: 'You Shook Me', artist: 'AC/DC'},
+        {title: 'Highway To Hell', artist: 'AC/DC'},
+        {title: 'Sweet Emotion', artist: 'Aerosmith'},
+        {title: 'Walk This Way', artist: 'Aerosmith'},
+        {title: 'Mama Kin', artist: 'Aerosmith'},
+        {title: 'Sweet Home Alabama', artist: 'Lynyrd Skynyrd'},
+        {title: 'Gimme Three Steps', artist: 'Lynyrd Skynyrd'},
+        {title: 'Free Bird', artist: 'Lynyrd Skynyrd'}],
+      showForm: true}
+  }
+
+  // Function to add new song to SongList
+  addSong = (song) => {
+    //DB Call would normally be here (axios or provider)
+
+    //Update State
+    let updateSongs = [song, ...this.state.songs]
+    this.setState({
+      songs:updateSongs
+    })
+  }
+
+  // Function to delete songs by filtering out all songs != songTitle
+  deleteSong = (songTitle) => {
+    //DB Delete Call would normally be here (axios or provider)
+
+    let filteredSongs = this.state.songs.filter(s => s.title !== songTitle)
+    this.setState({
+      songs:filteredSongs
+    })
+  }
+
+  //Function to display songs w/ delete button
+  renderSongs = () => {
+    return this.state.songs.map((s) => {
+      return (
+        <div>
+          <p>{s.title}</p>
+          <p>{s.artist}</p>
+          <button onClick={() => this.deleteSong(s.title)}>Delete</button>
+        </div>
+      );
+    });
+  };
+
+  //Function to toggle hiding/viewing the SongForm
+  toggleForm = () => {
+    this.setState({
+      showForm: !this.state.showForm
+    })
+  }
+
+  //You have to define render with a class component
+  render () {
+    return (
+      <div>
+        <h2>Song List</h2>
+        <button onClick={this.toggleForm}>{this.state.showForm ? 'Hide Form' : 'Show Form'}</button>
+        {this.renderSongs()}
+      </div>
+    )
+  }
+}
+
+export default ClassSongList
